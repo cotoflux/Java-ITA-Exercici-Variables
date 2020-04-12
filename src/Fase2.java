ad1a3d6ad10b4ede9e40ed0eb465cf47bec1cda4
cotoflux <guaupelu@hotmail.com>
2020-04-12 18:32:42 +0200

    Exercici fase 2 fet

---
+++ src/Fase2.java
@@ -0,0 +1,13 @@
+
+public class Fase2 {
+
+	public static void main(String[] args) {
+		int anyTraspas = 1948;
+		byte anysIntermedis = 4;
+		int aniversari = 1990;
+		int anysTraspasAniversari = (aniversari - anyTraspas) /4;
+		System.out.println(anysTraspasAniversari);
+
+	}
+
+}
