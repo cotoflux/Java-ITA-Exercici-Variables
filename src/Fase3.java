0bb3449f26ff04d3fa3706922a464791fc546841
cotoflux <guaupelu@hotmail.com>
2020-04-12 18:46:23 +0200

    Exercici Fase 3 fet

--- src/Fase3.java
+++ src/Fase3.java
@@ -1,4 +1,17 @@
 
 public class Fase3 {
 
+	public static void main(String args[]) {
+		int anyTraspas = 1948;
+		byte anysIntermedis = 4;
+		int aniversari = 1990;
+		int anysTraspasAniversari = (aniversari - anyTraspas) /4;
+		
+		for(int i = 0; i <= anysTraspasAniversari; i++) {
+			
+			anyTraspas = anyTraspas + anysIntermedis;
+			System.out.println(anyTraspas);
+		}
+	}
+	
 }
